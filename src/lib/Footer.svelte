b332246205e82bc0849130c8f59dcd03e306b1d7
Jonathan Schaaij <jonathan.schaaij@gmail.com>
2023-01-11 22:38:49 +0100

    Initial Commit

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="flex flex-col justify-center items-center p-8 sm:px-0 bg-background">
+<p>
+    All rights reserved © Jonathan Schaaij - {new Date().getFullYear()}
+</p>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Footer from "../lib/Footer.svelte";
+</script>
+
+<p class="text-green-500 hover:text-red-500 transition-all duration-500">Hi, my name is Jonathan Schaaij</p>
+
+<Footer/>
