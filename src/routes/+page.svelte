16caaf4e981fdd389779d70d6fdc199ac4402bf5
Jonathan Schaaij <jonathan.schaaij@gmail.com>
2024-03-01 15:11:45 +0100

    Added Goattracker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-    import Skills from "../lib/Skills.svelte";
-    import Hero from "$lib/Hero.svelte";
-    import Navbar from "$lib/Navbar.svelte";
-    import Footer from "../lib/Footer.svelte";
-    import AboutMe from "$lib/AboutMe.svelte";
+  import Skills from "../lib/Skills.svelte";
+  import Hero from "$lib/Hero.svelte";
+  import Navbar from "$lib/Navbar.svelte";
+  import Footer from "../lib/Footer.svelte";
+  import AboutMe from "$lib/AboutMe.svelte";
 
-    let bigLetters = `
+  let bigLetters = `
 ░░░░░██╗░█████╗░███╗░░██╗░█████╗░████████╗██╗░░██╗░█████╗░███╗░░██╗
 ░░░░░██║██╔══██╗████╗░██║██╔══██╗╚══██╔══╝██║░░██║██╔══██╗████╗░██║
 ░░░░░██║██║░░██║██╔██╗██║███████║░░░██║░░░███████║███████║██╔██╗██║
@@ -20,18 +20,23 @@
 ██████╔╝╚█████╔╝██║░░██║██║░░██║██║░░██║██║╚█████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░╚════╝░`;
 
-    console.log(bigLetters);
+  console.log(bigLetters);
 </script>
 
 <svelte:head>
-    <title>Hire Jonathan</title>
+  <script
+    data-goatcounter="https://jschaaij.goatcounter.com/count"
+    async
+    src="//gc.zgo.at/count.js"
+  ></script>
+  <title>Hire Jonathan</title>
 </svelte:head>
 
 <Navbar />
 
 <div class="w-screen h-screen snap-y snap-mandatory overflow-scroll">
-    <Hero />
-    <Skills />
-    <AboutMe />
-    <Footer />
+  <Hero />
+  <Skills />
+  <AboutMe />
+  <Footer />
 </div>
